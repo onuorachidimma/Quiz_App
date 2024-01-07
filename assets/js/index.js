575309be1668f3225221b9b3c39d22f970b8a5c0
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-01-07 15:08:44 +0100

    trying to add the calculate score functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,7 @@ const quizArray = [
         questionOption2: "High Tech Machine Learning",
         questionOption3: "Hyper Transfer Markup Language",
         questionOption4: "Hyperlink and Text Markup Language",
+        correctAnswer: 1
     },
     {
         question: "Which of the following is not a valid CSS property for text alignment?",
@@ -13,6 +14,7 @@ const quizArray = [
         questionOption2: "align-text",
         questionOption3: "text-justify",
         questionOption4: "text-indent",
+        correctAnswer: 2
     },
     {
         question: "What is the purpose of the JavaScript addEventListener method?",
@@ -20,6 +22,7 @@ const quizArray = [
         questionOption2: "To attach an event handler function to an HTML element",
         questionOption3: "To change the background color of an element",
         questionOption4: "To link external JavaScript files",
+        correctAnswer: 2
     },
     {
         question: "In CSS, how can you select all paragraphs with the class \"intro\"?",
@@ -27,6 +30,7 @@ const quizArray = [
         questionOption2: ".intro p",
         questionOption3: "#intro p",
         questionOption4: "intro.p",
+        correctAnswer: 2
     },
     {
         question: "What is the purpose of the <meta charset=\"UTF-8\"> tag in HTML?",
@@ -34,6 +38,7 @@ const quizArray = [
         questionOption2: "Define a meta description for search engines",
         questionOption3: "Embed external stylesheets",
         questionOption4: "Create a responsive design",
+        correctAnswer: 1
     },
     {
         question: "Which of the following is NOT a valid way to declare a variable in JavaScript?",
@@ -41,6 +46,7 @@ const quizArray = [
         questionOption2: "const myVar = 5;",
         questionOption3: "let myVar = 5;",
         questionOption4: "myVar = 5;",
+        correctAnswer: 4
     },
     {
         question: "What does the CSS property \"box-sizing: border-box;\" do?",
@@ -48,6 +54,7 @@ const quizArray = [
         questionOption2: "Includes padding and border in the element's total width and height",
         questionOption3: "Adjusts the margin of the box",
         questionOption4: "Centers the box on the page",
+        correctAnswer: 2
     },
     {
         question: "Which HTML tag is used for creating an unordered list?",
@@ -55,6 +62,7 @@ const quizArray = [
         questionOption2: "ol",
         questionOption3: "li",
         questionOption4: "list",
+        correctAnswer: 2
     },
     {
         question: "What is the purpose of the alt attribute in the HTML <img> tag?",
@@ -62,6 +70,7 @@ const quizArray = [
         questionOption2: "Provide alternative text for screen readers",
         questionOption3: "Set the image height and width",
         questionOption4: "Add a caption to the image",
+        correctAnswer: 2
     },
     {
         question: "How can you comment out multiple lines of code in JavaScript?",
@@ -69,6 +78,7 @@ const quizArray = [
         questionOption2: "/* This is a comment */",
         questionOption3: "# This is a comment",
         questionOption4: "-- This is a comment",
+        correctAnswer: 1
     }
 
 ]
@@ -97,16 +107,31 @@ let addQuizCard = () => {
     nextButton.addEventListener('click', showNextQuestion);
 };
 
+let calculateScore = () => {
+    let score = 0;
+    for (let i = 0; i < quizArray.length; i++) {
+        const selectedOption = document.querySelector(`input[name="option"]:checked`);
+        if (selectedOption) {
+            const selectedOptionIndex = parseInt(selectedOption.value);
+            if (selectedOptionIndex === quizArray[i].correctAnswer) {
+                score++;
+            }
+        }
+    }
+    return score;
+};
+
 let showNextQuestion = () => {
     currentQuizIndex++;
     if (currentQuizIndex < quizArray.length) {
         addQuizCard();
     } else {
         // Display this message when all questions are done
+        const score = calculateScore();
         quizContainer.innerHTML = `<fieldset>
         <legend>CONGRATULATIONS!!!</legend>
         <p>YOUR SCORE IS</p>
-        <h3>4/5</h3>
+        <h3>${score}/${quizArray.length}</h3>
         <button type="button">VIEW FEEDBACK</button>
         </fieldset>`;
         document.querySelector(".quizCardContainer").style.backgroundColor = "transparent";
@@ -116,3 +141,4 @@ let showNextQuestion = () => {
 };
 
 addQuizCard();
+
